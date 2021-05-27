8ce9dee9d7545a934144d96c5d3bd1952b302dd8
judongseok <wnehdtjr5@naver.com>
2021-05-27 23:00:23 +0900

    first commit

---
+++ nocona/hospitalModel.swift
@@ -0,0 +1,18 @@
+//
+//  hospitalModel.swift
+//  nocona
+//
+//  Created by judongseok on 2021/05/22.
+//
+
+import Foundation
+
+struct Hospital: Hashable {
+    // table Model
+    var adtFrDd: String?
+    var sgguNm: String?
+    var sidoNm: String?
+    var spclAdmTyCd: String?
+    var telno: String?
+    var yadmNm: String?
+}
